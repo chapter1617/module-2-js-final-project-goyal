ebecc0934d77d78eb4e2af502cbc9638fab98dd1
chapter1617 <goyal.mansi16@gmail.com>
2024-02-21 10:59:20 -0800

    script of random logic included

--- js/scriptwithrandomlogic.js
+++ js/scriptwithrandomlogic.js
@@ -11,21 +11,21 @@ const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
 
 // add background music 
-//const button = document.querySelector(".restart");
+const button = document.querySelector(".restart");
 
 const icon = document.querySelector("#button > i");
 const audio = document.querySelector("#quiz-play");
 const quizResSound = document.querySelector("#sucess");
 const nextBtn = document.querySelector(".next_btn");
 
-start_btn.addEventListener("click", () => {
+button.addEventListener("click", () => {
   if (audio.paused) {
     audio.volume = 0.2;
     audio.play();
   } else {
     audio.pause();
   }
-  //button.classList.add("fade");
+  button.classList.add("fade");
 });
 
 nextBtn.addEventListener('click' , ()=>{
@@ -33,45 +33,64 @@ nextBtn.addEventListener('click' , ()=>{
     audio.play();
 })
 // if startQuiz button clicked
-// start_btn.onclick = ()=>{
-//     info_box.classList.add("activeInfo"); //show info box
-// }
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //show info box
+}
 
 // if exitQuiz button clicked
 // exit_btn.onclick = ()=>{
 //     info_box.classList.remove("activeInfo"); //hide info box
 // }
 
-// function getRandomInt(min, max) { 
-//     let difference = max - min;
-//     let rand = Math.random();
-//     rand = Math.floor( rand * difference);
-//     rand = rand + min; 
-//     return rand;
-//     // min = Math.ceil(min); 
-//     // max = Math.floor(max); 
-//     // return Math.floor(Math.random() * (max - min + 1)) + min; 
-// }
+function shuffleArray(array) {
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+   
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    };
+    return array;
+ }
+
+function getRandomItem(arr,length) {
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * length);
+
+    // get random item
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const array = [0,1,2,3,4,5,6,7,8,9];
+
+let rdArr = shuffleArray(array);
+//const res = getRandomItem(rdArr);
+//console.log('random val ------'+res);
 
 // if continueQuiz button clicked
 start_btn.onclick = ()=>{
     info_box.classList.remove("activeInfo"); //hide info box
     quiz_box.classList.add("activeQuiz"); //show quiz box
-    
-    //const result = getRandomItem(array);
-    showQuestions(0); //calling showQestions function
+    showQuetions(1,getRandomItem(rdArr,9)); //calling showQestions function
     queCounter(1); //passing 1 parameter to queCounter
     startTimer(15); //calling startTimer function
     startTimerLine(0); //calling startTimerLine function
 }
 
 let timeValue =  15;
-let que_count = 0;
+let que_count = 1;
 let que_numb = 1;
 let userScore = 0;
 let counter;
 let counterLine;
 let widthValue = 0;
+let len = questions.length ; 
 
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
@@ -85,8 +104,7 @@ restart_quiz.onclick = ()=>{
     que_numb = 1;
     userScore = 0;
     widthValue = 0;
-    //const result = getRandomItem(array);
-    showQuestions(que_count); //calling showQestions function
+    showQuetions(que_count,getRandomItem(rdArr,9)); //calling showQestions function
     queCounter(que_numb); //passing que_numb value to queCounter
     clearInterval(counter); //clear counter
     clearInterval(counterLine); //clear counterLine
@@ -111,17 +129,16 @@ quit_quiz.onclick = ()=>{
 const next_btn = document.querySelector("footer .next_btn");
 const bottom_ques_counter = document.querySelector("footer .total_que");
 
-
 // if Next Que button clicked
 next_btn.onclick = ()=>{
-    //ques_num = getRandomInt(0,4);
-    
-    
-    if(que_count < questions.length-1){ //if question count is less than total question length
+   
+    //const res = getRandomItem(rdArr);
+    //console.log("inside next"+getRandomInt(0,4));
+    if(que_count < questions.length){ //if question count is less than total question length
         que_count++; //increment the que_count value
         que_numb++; //increment the que_numb value
-        //const result = getRandomItem(array);
-        showQuestions(que_count); //calling showQestions function
+        len--;
+        showQuetions(que_count,getRandomItem(rdArr,len)); //calling showQestions function
         queCounter(que_numb); //passing que_numb value to queCounter
         clearInterval(counter); //clear counter
         clearInterval(counterLine); //clear counterLine
@@ -136,18 +153,17 @@ next_btn.onclick = ()=>{
         showResult(); //calling showResult function
     }
 }
-let quesNum = 0 ; 
+let ques_num = 0 ; 
 // getting questions and options from array
-function showQuestions(index){
+function showQuetions(index,num){
     const que_text = document.querySelector(".que_text");
-    console.log("inside show ques..."+index);
-    
+    console.log("inside show ques"+num);
     //creating a new span and div tag for question and option and passing the value using array index
-    let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
-    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    let que_tag = '<span>'+ index + ". " + questions[num].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[num].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[num].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[num].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[num].options[3] +'</span></div>';
     que_text.innerHTML = que_tag; //adding new span tag inside que_tag
     option_list.innerHTML = option_tag; //adding new div tag inside option_tag
     
@@ -155,9 +171,9 @@ function showQuestions(index){
 
     // set onclick attribute to all available options
     for(i=0; i < option.length; i++){
-        //quesNum = num ; 
-        //console.log(index + "-----"+ quesNum );
-        option[i].setAttribute("onclick", "optionSelected(this)");
+        ques_num = num ; 
+        console.log(index + "-----"+ num );
+        option[i].setAttribute("onclick", "optionSelected(this,ques_num)");
     }
 }
 // creating the new div tags which for icons
@@ -165,12 +181,11 @@ let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
 let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
 
 //if user clicked on option
-function optionSelected(answer){
-    //console.log(+answer +"answer---->num"+ num);
+function optionSelected(answer,num){
     clearInterval(counter); //clear counter
     clearInterval(counterLine); //clear counterLine
     let userAns = answer.textContent; //getting user selected option
-    let correcAns = questions[que_count].answer; //getting correct answer from array
+    let correcAns = questions[num].answer; //getting correct answer from array
     const allOptions = option_list.children.length; //getting all option items
     
     if(userAns == correcAns){ //if user selected option is equal to array's correct answer
@@ -209,7 +224,7 @@ function showResult(){
         let scoreTag = '<span>Congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
         if (audio.paused) {
-            quizResSound.setAttribute('src','assets/tadaa.mp3');
+            quizResSound.setAttribute('src','tadaa.mp3');
             quizResSound.load();
             quizResSound.play();
             
@@ -220,7 +235,7 @@ function showResult(){
         let scoreTag = '<span>Nice 😎, Almost there . You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
         if (audio.paused) {
-            quizResSound.setAttribute('src','assets/goodresult.mp3');
+            quizResSound.setAttribute('src','goodresult.mp3');
             quizResSound.load();
             quizResSound.play();
             
@@ -231,7 +246,7 @@ function showResult(){
         let scoreTag = '<span>Good work, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
         if (audio.paused) {
-            quizResSound.setAttribute('src','assets/goodresult.mp3');
+            quizResSound.setAttribute('src','goodresult.mp3');
             quizResSound.load();
             quizResSound.play();
             
@@ -242,7 +257,7 @@ function showResult(){
         let scoreTag = '<span>😐 Work harder. You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
         if (audio.paused) {
-            quizResSound.setAttribute('src','assets/game-fail.mp3');
+            quizResSound.setAttribute('src','game-fail.mp3');
              quizResSound.load();
              quizResSound.play();
             
