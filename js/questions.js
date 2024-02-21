a7ba904d8499fce5918f9079dd15a43d3fa553c8
chapter1617 <goyal.mansi16@gmail.com>
2024-02-20 18:17:48 -0800

    js project code 1

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How do you write 'Hello World' in an alert box?",
+    answer: "alert('Hello World');",
+    options: [
+      "msg('Hello World')",
+      "msgBox('Hello World');",
+      "alertBox('Hello World');",
+      "alert('Hello World');"
+    ]
+  },
+  {
+    numb: 7,
+    question: "How to empty an array in JavaScript?",
+    answer: "arrayList.length=0",
+    options: [
+      "arrayList[]",
+      "arrayList(0)",
+      "arrayList.length=0",
+      "arrayList.len(0)"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What function to add an element at the begining of an array and one at the end?",
+    answer: "unshift,push",
+    options: [
+      "push,unshift",
+      "unshift,push",
+      "first,push",
+      "unshift,last"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What would following code return? console.log(typeof typeof 1);",
+    answer: "string",
+    options: [
+      "string",
+      "number",
+      "Syntax Error",
+      "undefined"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which software company developed JavaScript?",
+    answer: "Netscape",
+    options: [
+      "Mozilla",
+      "Netscape",
+      "Sun Microsystems",
+      "Oracle"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
